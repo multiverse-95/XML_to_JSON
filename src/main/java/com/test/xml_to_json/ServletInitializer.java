4af54020375dee16462055d021b2fcab77b3eb42
multiverse-95 <vyvalkov@yandex.ru>
2022-10-23 15:13:46 +0700

    Initial commit

---
+++ src/main/java/com/test/xml_to_json/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.test.xml_to_json;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(XmlToJsonApplication.class);
+    }
+
+}
